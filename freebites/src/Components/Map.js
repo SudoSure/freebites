67bdb3c3d8de8824a0b5b5bd2f57a26af65b2ca7
gregoryweber <gregory1@weberdao.org>
2024-04-06 15:46:13 -0700

    added location

--- freebites/src/Components/Map.js
+++ freebites/src/Components/Map.js
@@ -1,29 +1,91 @@
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps"; // remove PROVIDER_GOOGLE import if not using Google Maps
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import Device from "expo-device";
+import * as Location from "expo-location";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+
+export default function Map() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    const [position, setPosition] = useState({
+        latitude: 10,
+        longitude: 10,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+    });
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS === "android" && !Device.isDevice) {
+                setErrorMsg(
+                    "Oops, this will not work on Snack in an Android Emulator. Try it on your device!"
+                );
+                return;
+            }
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            setPosition({
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.0421,
+                longitudeDelta: 0.0421,
+            });
+        })();
+    }, []);
+
+    let text = "Waiting..";
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
+    return (
+        <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            initialRegion={position}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            followsUserLocation={true}
+            showsCompass={true}
+            scrollEnabled={true}
+            zoomEnabled={true}
+            pitchEnabled={true}
+            rotateEnabled={true}>
+            <Marker
+                title="Yor are here"
+                description="This is a description"
+                coordinate={position}
+            />
+        </MapView>
+    );
+    // return (
+    //     <View>
+    //         <Text>{text}</Text>
+    //     </View>
+    // );
+}
 
 const styles = StyleSheet.create({
     container: {
-        ...StyleSheet.absoluteFillObject,
-        height: 800,
-        width: 400,
-        justifyContent: "flex-end",
+        flex: 1,
         alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    paragraph: {
+        fontSize: 18,
+        textAlign: "center",
     },
     map: {
         ...StyleSheet.absoluteFillObject,
     },
 });
-
-export default () => (
-    <View style={styles.container}>
-        <MapView
-            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-            style={styles.map}
-            region={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-            }}></MapView>
-    </View>
-);
