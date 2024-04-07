c090630aaf57ef9d7575f9eeb48291aabaf2d3b4
T.N <35242977+SudoSure@users.noreply.github.com>
2024-04-06 17:03:25 -0700

    fixed Map.js

--- freebites/src/Components/Map.js
+++ freebites/src/Components/Map.js
@@ -1,41 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Platform, Text, View, StyleSheet } from "react-native";
-import Device from "expo-device";
 import * as Location from "expo-location";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export default function Map() {
+    const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
 
-    const [position, setPosition] = useState(null);
+    const [position, setPosition] = useState({
+        latitude: 10,
+        longitude: 10,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+    });
 
     useEffect(() => {
         (async () => {
-            if (Platform.OS === "android" && !Device.isDevice) {
-                setErrorMsg(
-                    "Oops, this will not work on Snack in an Android Emulator. Try it on your device!"
-                );
-                setPosition({
-                    latitude: 37.785834,
-                    longitude: -122.406417,
-                    latitudeDelta: 0.0421,
-                    longitudeDelta: 0.0421,
-                });
-                return;
-            }
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== "granted") {
                 setErrorMsg("Permission to access location was denied");
-                setPosition({
-                    latitude: 37.785834,
-                    longitude: -122.406417,
-                    latitudeDelta: 0.0421,
-                    longitudeDelta: 0.0421,
-                });
                 return;
             }
 
-            const location = await Location.getCurrentPositionAsync({});
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
             setPosition({
                 latitude: location.coords.latitude,
                 longitude: location.coords.longitude,
@@ -45,9 +33,14 @@ export default function Map() {
         })();
     }, []);
 
-    return position == null ? (
-        <View />
-    ) : (
+    let text = "Waiting..";
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
+    return (
         <MapView
             provider={PROVIDER_GOOGLE}
             style={styles.map}
@@ -61,7 +54,7 @@ export default function Map() {
             pitchEnabled={true}
             rotateEnabled={true}>
             <Marker
-                title="Yor are here"
+                title="You are here"
                 description="This is a description"
                 coordinate={position}
             />
